5e59226fbc4c0dd3f756f6e443c3ba9308858d60
ripBlaster <149702127+ripBlaster@users.noreply.github.com>
2023-12-31 21:26:29 -0300

    Update HardBan.lua

--- HardBan.lua
+++ HardBan.lua
@@ -4,8 +4,5 @@ local Ban = {
 		[1672825547] = {Motivo = "¿"}, -- cobaia do p3u
                 [1513291266] = {Motivo = "Deu gg"} -- izi
             },
-     --['Ban_Amigos'] = {
-	      --[0] = {Motivo = ""} --
-	    --},
 	}
 return Ban 
