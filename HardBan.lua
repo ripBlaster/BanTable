21b636df38e50a0b126c6269bcc979daf6dee671
ripBlaster <149702127+ripBlaster@users.noreply.github.com>
2023-12-31 21:12:02 -0300

    HardBan.lua

--- HardBan.lua
+++ HardBan.lua
@@ -1,13 +1,11 @@
-local local Ban = {
+local Ban = {
 	['Ban'] = { 
 		[3615676414] = {Motivo = "flw"}, -- p3u
 		[1672825547] = {Motivo = "¿"}, -- cobaia do p3u
     [1513291266] = {Motivo = "Deu gg"} - izi
 	},
-	['Ban_Amigos'] = {
-	[0] = {Motivo = ""} --
-
-	},
-
+	---['Ban_Amigos'] = {
+	--[0] = {Motivo = ""} --
+	--},
 }
 return Ban 
