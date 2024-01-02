9d727095b24cb7c92ad1910c5e6f3ba0b465a717
ripBlaster <149702127+ripBlaster@users.noreply.github.com>
2024-01-01 21:53:52 -0300

    Update HardBan.lua

--- HardBan.lua
+++ HardBan.lua
@@ -3,6 +3,26 @@ local Ban = {
 		[3615676414] = {Motivo = "flw"}, -- p3u
 		[1672825547] = {Motivo = "¿"}, -- cobaia do p3u
                 [1513291266] = {Motivo = "Deu gg"} -- izi
+		[4603131888] = {Motivo = "flw"} -- Não sei mas tá de ban 
+	        [2662429331] = {Motivo = "flw"} -- Não sei mas tá de ban 
+	        [5041427384] = {Motivo = "flw"} -- Não sei mas tá de ban 
+	        [1713837623] = {Motivo = "flw"} -- Não sei mas tá de ban 
+	        [1549288505] = {Motivo = "flw"} -- Não sei mas tá de ban 
+	        [4883452764] = {Motivo = "flw"} -- Não sei mas tá de ban 
+	        [4748033327] = {Motivo = "flw"} -- Não sei mas tá de ban 
+		[4255254973] = {Motivo = "teste"} -- teste favor tirar
+		
             },
-	}
+	['Ban_Amigos'] = {
+	[4603131888] = {Motivo = "flw"} -- Não sei mas tá de ban 
+	[2662429331] = {Motivo = "flw"} -- Não sei mas tá de ban 
+	[5041427384] = {Motivo = "flw"} -- Não sei mas tá de ban 
+	[1713837623] = {Motivo = "flw"} -- Não sei mas tá de ban 
+	[1549288505] = {Motivo = "flw"} -- Não sei mas tá de ban 
+	[4883452764] = {Motivo = "flw"} -- Não sei mas tá de ban 
+	[4748033327] = {Motivo = "flw"} -- Não sei mas tá de ban 
+	[4526342082] = {Motivo = "flw"} -- Não sei mas tá de ban 
+		
+	},
+}
 return Ban 
