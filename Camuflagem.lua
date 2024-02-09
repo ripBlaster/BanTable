7ca44a05ef66495be5e0fb54059178137638008b
ripBlaster <149702127+ripBlaster@users.noreply.github.com>
2024-02-09 20:54:33 -0300

    Update Camuflagem.lua

--- Camuflagem.lua
+++ Camuflagem.lua
@@ -2,7 +2,7 @@ local CamoEvent = Instance.new("RemoteEvent") CamoEvent.Name = "CamoEvent" CamoE
 local Players = game:GetService("Players")
 local GetNameFromUserIdAsync = Players.GetNameFromUserIdAsync
 
-local Perm = {"xandecat11", "ronaldofenomeno20111","NuelNoCra", "Secbucheta65788", "NicolasArrua58", "MarcoAntonioTelesRi", "ed3vsrs5", "Ragnarok_xxxxxxx"}
+local Perm = {"xandecat11", "ronaldofenomeno20111","NuelNoCra", "Secbucheta65788", "NicolasArrua58", "MarcoAntonioTelesRi", "ed3vsrs5", "Ragnarok_xxxxxxx", "Gui_TgF"}
 
 local function randomUser()
 	local randomId = math.random(1, 2000000000)
