20da6ffba992cc4d44c9d6a637eaef984e045d4e
ripBlaster <149702127+ripBlaster@users.noreply.github.com>
2024-05-12 10:54:25 -0300

    Update Hardban2.0.lua

--- Hardban2.0.lua
+++ Hardban2.0.lua
@@ -1,30 +1,17 @@
 local Ban = {
 	['Ban'] = { 
-	[4603131888] = {Motivo = "flw"},
 	[2662429331] = {Motivo = "flw"},
 	[5041427384] = {Motivo = "flw"},
 	[1713837623] = {Motivo = "flw"},
 	[1549288505] = {Motivo = "flw"},
-	[4883452764] = {Motivo = "flw"},
 	[4748033327] = {Motivo = "flw"},
-	[4526342082] = {Motivo = "flw"},
-	[3726410772] = {Motivo = "flw"},
-	[4667788005] = {Motivo = "flw"},
-        [1072016101] = {Motivo = "flw"}, -- abuso de all
-	[5274560756] = {Motivo = "flw"}, -- all
 	},
 	['Ban_Amigos'] = {
-	[4603131888] = {Nick = "if7f7fi"},
 	[2662429331] = {Nick = "FLAVIOGABRIEI"},
 	[5041427384] = {Nick = "esther_100900"},
 	[1713837623] = {Nick = "santypropayer14"},
 	[1549288505] = {Nick = "santypropayer"},
-	[4883452764] = {Nick = "mrchicken1392"},
 	[4748033327] = {Nick = "TopGhostBr"},
-	[4526342082] = {Nick = "Abelaq3541"},
-	[3726410772] = {Nick = "Marceloavares"},
-	[4667788005] = {Nick = "reidelas082384"},
-        [1072016101] = {Nick = "faridipro"},
 	},
 
 }
